fcdf98b8c819ddc9ae3b5f3e0eeebb665ed9f3e1
Anton Kosiakov <anton.kosyakov@typefox.io>
2019-07-04 14:18:29 +0200

    onDebugResolve
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,11 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage('onDebugInitialConfigurations');
-	vscode.debug.registerDebugConfigurationProvider('mock', {
-		provideDebugConfigurations: () => {
-			vscode.window.showInformationMessage('provideDebugConfigurations');
-			return [{
-				"type": "mock",
-				"request": "launch",
-				"name": "Ask for file name",
-				"program": "${workspaceFolder}/${command:AskForProgramName}",
-				"stopOnEntry": true
-			}];
+	vscode.window.showInformationMessage('onDebugResolve');
+	vscode.debug.registerDebugConfigurationProvider('*', {
+		resolveDebugConfiguration: (_, config) => {
+			vscode.window.showInformationMessage('resolveDebugConfiguration');
+			return config;
 		}
 	});
 }
