4300fb56e0c2c4ea7adbf3d705367532ae879fdc
Anton Kosiakov <anton.kosyakov@typefox.io>
2019-07-04 16:28:02 +0200

    onDebugAdapterProtocolTracker:node
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,16 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage('onDebugAdapterProtocolTracker');
-	vscode.debug.registerDebugAdapterTrackerFactory('*', {
+	vscode.window.showInformationMessage('onDebugAdapterProtocolTracker:node');
+	vscode.debug.registerDebugAdapterTrackerFactory('node', {
 		createDebugAdapterTracker(session: vscode.DebugSession) {
-			vscode.window.showInformationMessage('createDebugAdapterTracker');
+			vscode.window.showInformationMessage('createDebugAdapterTracker:node');
+			return {};
+		}
+	});
+	vscode.debug.registerDebugAdapterTrackerFactory('node2', {
+		createDebugAdapterTracker(session: vscode.DebugSession) {
+			vscode.window.showInformationMessage('createDebugAdapterTracker:node2');
 			return {};
 		}
 	});
