308769ca4653b0c60d11b1eed922766cb98567b3
Anton Kosiakov <anton.kosyakov@typefox.io>
2019-07-04 14:10:52 +0200

    onDebugInitialConfigurations
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,19 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage('onDebug');
+	vscode.window.showInformationMessage('onDebugInitialConfigurations');
+	vscode.debug.registerDebugConfigurationProvider('mock', {
+		provideDebugConfigurations: () => {
+			vscode.window.showInformationMessage('provideDebugConfigurations');
+			return [{
+				"type": "mock",
+				"request": "launch",
+				"name": "Ask for file name",
+				"program": "${workspaceFolder}/${command:AskForProgramName}",
+				"stopOnEntry": true
+			}];
+		}
+	});
 }
 
 // this method is called when your extension is deactivated
